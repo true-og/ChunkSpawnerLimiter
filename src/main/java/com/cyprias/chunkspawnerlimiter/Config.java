5f511078ed4cdc613e891c83b301b57456d5a722
Omer Oreg <sarhatabaot1@gmail.com>
2022-02-12 11:11:54 +0200

    Renamed config to fileConfiguration

--- src/main/java/com/cyprias/chunkspawnerlimiter/Config.java
+++ src/main/java/com/cyprias/chunkspawnerlimiter/Config.java
@@ -3,52 +3,44 @@ package com.cyprias.chunkspawnerlimiter;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import java.util.List;
-import java.util.Objects;
 
 public class Config {
-	private static FileConfiguration config = ChunkSpawnerLimiter.getInstance().getConfig();
-	public static List<String> EXCLUDED_WORLDS = config.getStringList("excluded-worlds");
+	private static FileConfiguration fileConfiguration = ChunkSpawnerLimiter.getInstance().getConfig();
+	public static List<String> EXCLUDED_WORLDS = fileConfiguration.getStringList("excluded-worlds");
 
 	private Config() {
 		throw new UnsupportedOperationException();
 	}
 
-	public static void reload() {
-		config = ChunkSpawnerLimiter.getInstance().getConfig();
-	}
 
-	public static boolean getBoolean(String property) {
-		return ChunkSpawnerLimiter.getInstance().getConfig().getBoolean(property);
+	public static void reload() {
+		fileConfiguration = ChunkSpawnerLimiter.getInstance().getConfig();
 	}
 
 	public static int getEntityLimit(String entityType) {
-		return config.getInt("entities." + entityType);
+		return fileConfiguration.getInt("entities." + entityType);
 	}
 
 	public static boolean isSpawnReason(String reason) {
-		return config.getBoolean("spawn-reasons." + reason);
-	}
-
-	public static String getString(String property, Object... args) {
-		return String.format(Objects.requireNonNull(config.getString(property)), args);
+		return fileConfiguration.getBoolean("spawn-reasons." + reason);
 	}
 
 	public static boolean contains(String property) {
-		return config.contains(property);
+		return fileConfiguration.contains(property);
 	}
 
 	public static class Properties {
 		private static String path = "properties.";
-		public static boolean DEBUG_MESSAGES = config.getBoolean(path + "debug-messages");
-		public static boolean CHECK_CHUNK_LOAD = config.getBoolean(path + "check-chunk-load");
-		public static boolean CHECK_CHUNK_UNLOAD = config.getBoolean(path + "check-chunk-unload");
-		public static boolean ACTIVE_INSPECTIONS = config.getBoolean(path + "active-inspections");
-		public static boolean WATCH_CREATURE_SPAWNS = config.getBoolean(path + "watch-creature-spawns");
-		public static int CHECK_SURROUNDING_CHUNKS = config.getInt(path + "check-surrounding-chunks");
-		public static int INSPECTION_FREQUENCY = config.getInt(path + "inspection-frequency");
-		public static boolean NOTIFY_PLAYERS = config.getBoolean(path + "notify-players");
-		public static boolean PRESERVE_NAMED_ENTITIES = config.getBoolean(path + "preserve-named-entities");
-		public static List<String> IGNORE_METADATA = config.getStringList(path + "ignore-metadata");
+		public static boolean DEBUG_MESSAGES = fileConfiguration.getBoolean(path + "debug-messages");
+		public static boolean CHECK_CHUNK_LOAD = fileConfiguration.getBoolean(path + "check-chunk-load");
+		public static boolean CHECK_CHUNK_UNLOAD = fileConfiguration.getBoolean(path + "check-chunk-unload");
+		public static boolean ACTIVE_INSPECTIONS = fileConfiguration.getBoolean(path + "active-inspections");
+		public static boolean WATCH_CREATURE_SPAWNS = fileConfiguration.getBoolean(path + "watch-creature-spawns");
+		public static int CHECK_SURROUNDING_CHUNKS = fileConfiguration.getInt(path + "check-surrounding-chunks");
+		public static int INSPECTION_FREQUENCY = fileConfiguration.getInt(path + "inspection-frequency");
+		public static boolean NOTIFY_PLAYERS = fileConfiguration.getBoolean(path + "notify-players");
+		public static boolean PRESERVE_NAMED_ENTITIES = fileConfiguration.getBoolean(path + "preserve-named-entities");
+		public static List<String> IGNORE_METADATA = fileConfiguration.getStringList(path + "ignore-metadata");
 
 		private Properties() {
 			throw new UnsupportedOperationException();
@@ -57,8 +49,8 @@ public class Config {
 
 	public static class Messages {
 		private static String path = "messages.";
-		public static String REMOVED_ENTITIES = config.getString(path + "removedEntities");
-		public static String RELOADED_CONFIG = config.getString(path + "reloadedConfig", "&cReloaded csl config.");
+		public static String REMOVED_ENTITIES = fileConfiguration.getString(path + "removedEntities");
+		public static String RELOADED_CONFIG = fileConfiguration.getString(path + "reloadedConfig", "&cReloaded csl config.");
 
 		private Messages() {
 			throw new UnsupportedOperationException();
