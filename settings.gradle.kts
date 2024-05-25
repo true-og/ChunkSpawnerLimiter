cc044dfe340e3cd5e31073a6ad47f477cf4085d7
Omer Oreg <sarhatabaot1@gmail.com>
2024-05-25 11:26:19 +0300

    Port to gradle & update action
    Also updated license with current year (2024)

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+rootProject.name = "ChunkSpawnerLimiter"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            library("spigot-api", "org.spigotmc:spigot-api:1.14.4-R0.1-SNAPSHOT")
+            library("bstats", "org.bstats:bstats-bukkit:3.0.2")
+            library("acf", "co.aikar:acf-paper:0.5.1-SNAPSHOT")
+            library("annotations", "org.jetbrains:annotations:24.0.1")
+            library("nbt-api", "de.tr7zw:item-nbt-api:2.12.4")
+
+            plugin("plugin-yml","net.minecrell.plugin-yml.bukkit").version("0.6.0")
+            plugin("shadow","com.github.johnrengelman.shadow").version("8.1.1")
+        }
+    }
+}
